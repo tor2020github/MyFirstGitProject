7af97da3ebbd559a330d7eb9ae1ad878d88fec6d
温振刚 <opnicorobin@hotmail.com>
2020-03-03 21:26:40 +0800

    还原

--- MyFirstGitProject/Cat.cs
+++ MyFirstGitProject/Cat.cs
@@ -14,7 +14,6 @@ namespace MyFirstGitProject
         public int Age { get; set; }
         public override string ToString()
         {
-            //修复了一个共同版本都有的bug
             return "名字：" + Name + " 颜色：" + Color + " 年龄：" + Age;
         }
     }
