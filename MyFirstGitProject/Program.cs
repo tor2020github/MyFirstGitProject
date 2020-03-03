b29688f0f7da78d1278054c863e3141806433764
温振刚 <opnicorobin@hotmail.com>
2020-03-03 21:09:04 +0800

    为Cat类添加颜色属性，并修改main方法

--- MyFirstGitProject/Program.cs
+++ MyFirstGitProject/Program.cs
@@ -10,7 +10,7 @@ namespace MyFirstGitProject
     {
         static void Main(string[] args)
         {
-            var Tom = new Cat() { Id = 1, Name = "Tom" };
+            var Tom = new Cat() { Id = 1, Name = "Tom", Color = "蓝色" };
             Console.WriteLine(Tom.ToString());
             Console.ReadLine();
         }
